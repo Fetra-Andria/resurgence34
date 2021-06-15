0dfc0d563655c233f0aafb6099bc95a407105250
noeljuillerat <noel.juillerat@gmail.com>
2021-06-15 14:23:20 +0200

    SCSS mis en place et fonctionnel

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,83 +1,77 @@
-import React from 'react';
-import '../Styles/Footer.css';
+import React from "react";
+//import '../Styles/Footer.css';
 //import { Button } from '../Components/Button';
-import { Link } from 'react-router-dom';
-import {
-  FaFacebook,
-  FaInstagram,
-  FaTwitter,
-  FaLinkedin
-} from 'react-icons/fa';
-import { DiFsharp } from 'react-icons/di';
+import { Link } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa";
+import { DiFsharp } from "react-icons/di";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+    <div className="footer-container">
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>About Us</h2>
-            <Link to='/a-propos-de-nous'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
+            <Link to="/a-propos-de-nous">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
           </div>
-          <div className='footer-link-items'>
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
-            <Link to='/contact'>Contact</Link>
-            <Link to='/'>Support</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/">Support</Link>
           </div>
         </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+            <Link to="/">Youtube</Link>
+            <Link to="/">Twitter</Link>
           </div>
         </div>
       </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-              <DiFsharp className='navbar-icon' />
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              <DiFsharp className="navbar-icon" />
               RESURGENCE 34
             </Link>
           </div>
-          <small className='website-rights'>RESURGENCE 34 © 2020</small>
-          <div className='social-icons'>
+          <small className="website-rights">RESURGENCE 34 © 2020</small>
+          <div className="social-icons">
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
             >
               <FaFacebook />
             </Link>
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
             >
               <FaInstagram />
             </Link>
-            
+
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
             >
               <FaTwitter />
             </Link>
             <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
             >
               <FaLinkedin />
             </Link>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,74 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import { IconContext } from 'react-icons';
-import { DiFsharp } from 'react-icons/di';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import '../Styles/Navbar.css';
-import { Button } from './Button';
+import React, { useEffect, useState } from "react";
+import { IconContext } from "react-icons";
+import { DiFsharp } from "react-icons/di";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+/* import '../Styles/Navbar.css'; */
+import { Button } from "./Button";
 
 const Navbar = () => {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(true); 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(true);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-            setButton(false)
-        }
-        else {
-            setButton(true)
-        }
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
+  };
 
-    useEffect(() => {
-        showButton();
-        window.addEventListener('resize', showButton);
-    },[])
-    return (
-        <IconContext.Provider value={{ color : '#fff'}}>
-            <nav className='navbar'>
-                <div className='navbar-container container'>
-                    <Link to='/' className='navbar-logo' >
-                        RESURGENCE34
-                        <DiFsharp className='navbar-icon'/>
-                    </Link>
-                    <div className='menu-icon' onClick={handleClick}>
-                        {click ? <FaTimes className='fa-times' /> : <FaBars className='fa-bars' />}
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                                Accueil
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                                Services
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/a-propos-de-nous' className='nav-links' onClick={closeMobileMenu}>
-                                A-propos-de-nous
-                            </Link>
-                        </li>
-                        <li className='nav-btn'>
-                            {button ? (
-                                <Link to='/contact' className='btn-link'>
-                                    <Button buttonStyle='btn--outline'>CONTACT</Button>
-                                </Link>
-                            ) : (
-                                <Link to='/contact' className='btn-link' onClick={closeMobileMenu}>
-                                    <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>CONTACT</Button>
-                                </Link>
-                            )}
-                        </li>
-                    </ul>
-                </div>
-             </nav>
-        </IconContext.Provider>
-    );
+  useEffect(() => {
+    showButton();
+    window.addEventListener("resize", showButton);
+  }, []);
+  return (
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <nav className="navbar">
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo">
+            RESURGENCE34
+            <DiFsharp className="navbar-icon" />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? (
+              <FaTimes className="fa-times" />
+            ) : (
+              <FaBars className="fa-bars" />
+            )}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Accueil
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/a-propos-de-nous"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                A-propos-de-nous
+              </Link>
+            </li>
+            <li className="nav-btn">
+              {button ? (
+                <Link to="/contact" className="btn-link">
+                  <Button buttonStyle="btn--outline">CONTACT</Button>
+                </Link>
+              ) : (
+                <Link
+                  to="/contact"
+                  className="btn-link"
+                  onClick={closeMobileMenu}
+                >
+                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                    CONTACT
+                  </Button>
+                </Link>
+              )}
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </IconContext.Provider>
+  );
 };
 
 export default Navbar;
