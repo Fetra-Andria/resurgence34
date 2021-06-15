0c58757fcfb2c5df23312b72bb96123c23b4b62e
noeljuillerat <noel.juillerat@gmail.com>
2021-06-15 11:10:22 +0200

    mise en place style

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './Styles/App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Pages/Home';
-import Services from './Pages/Services';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
+import React from "react";
+/* import './Styles/App.css'; */
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/services' component={Services} />
-        <Route path='/a-propos-de-nous' component={About} />
-        <Route path='/contact' component={Contact} />
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/a-propos-de-nous" component={About} />
+        <Route path="/contact" component={Contact} />
       </Switch>
       <Footer />
     </Router>
